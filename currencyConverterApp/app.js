75992ad0f13ba09cf6566da33df994c87284746f
Sayan Maity <cse21109@cemk.ac.in>
2024-09-01 19:20:58 +0530

    failed to complete the project

--- currencyConverterApp/app.js
+++ currencyConverterApp/app.js
@@ -1,32 +1,64 @@
 const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
-let amountClass=document.querySelector(".amount")
-let amountValue=amountClass.ariaValueMax;
-let dropDownSelect=document.querySelectorAll(".dropdown select")
+    "https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurr}";
 
+    let Uri="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/v1/{endpoint}"
+    let dropDownSelect = document.querySelectorAll(".dropdown select")
+let button = document.querySelector("form button");
+let fromCurr=document.querySelector(".from select")
+let toCurr=document.querySelector(".to select")
+const msg = document.querySelector(".msg");
 
-for(let select of dropDownSelect){
-    for (let codes in countryList){
-        let newElement=document.createElement("option");
+for (let select of dropDownSelect) {
+    for (let codes in countryList) {
+        let newElement = document.createElement("option");
         newElement.innerText = codes;
         newElement.value = codes;
-        if(select.name==="from" && codes==="USD"){
-            newElement.selected="selected"
-        }else if(select.name==="To" && codes==="INR"){
-            newElement.selected="selected"
+        if (select.name === "from" && codes === "USD") {
+            newElement.selected = "selected"
+        } else if (select.name === "To" && codes === "INR") {
+            newElement.selected = "selected"
         }
         select.append(newElement)
     }
- select.addEventListener("change",(event)=>{
-    updateFlag(event.target)
- })
+    select.addEventListener("change", (event) => {
+        updateFlag(event.target)
+    })
 }
 
-let updateFlag=(element)=>{
-    const currecyCode=element.value;
-    let countryValue=countryList[currecyCode];
-    let newImageSrc=`https://flagsapi.com/${countryValue}/flat/64.png`
-    let imgageId=element.parentElement.querySelector("img")
-    imgageId.src=newImageSrc;
-    
+let updateFlag = (element) => {
+    const currecyCode = element.value;
+    let countryValue = countryList[currecyCode];
+    let newImageSrc = `https://flagsapi.com/${countryValue}/flat/64.png`
+    let imgageId = element.parentElement.querySelector("img")
+    imgageId.src = newImageSrc;
+
 }
+let updateExchange=async()=>{
+    let amountInput = document.querySelector(".amount input");
+    let amountValue = amountInput.value;
+    if (amountValue === '' || amountValue < 0) {
+        amountValue = 1;
+        amountInput.value = "1";
+    }
+    // console.log(fromCurr.value,toCurr.value);
+    try{
+    const response = await fetch(BASE_URL);
+    console.log(response)
+    const data = await response.json();
+    // let rate=json[fromCurr][toCurr]
+    }catch(err){
+        console.log(err)
+    }
+    // let finalAmount = amtVal * rate;
+    // msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+}
+
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+    updateExchange();
+})
+
+window.addEventListener("load", () => {
+    updateExchange();
+  });
+
--- index.js
+++ index.js
@@ -1,83 +1,117 @@
-// let promise=new Promise((resolve,reject)=>{
-//     console.log("I am a promise")
-//     reject("some error occured here");
-// })
+// // let promise=new Promise((resolve,reject)=>{
+// //     console.log("I am a promise")
+// //     reject("some error occured here");
+// // })
 
-// Promise usage
+// // Promise usage
 
 
-// const getData=()=>{
-//     return new Promise((resolve,reject)=>{
-//         console.log("I am a promise")
-//         // resolve("Success message");
-//         reject("Some error occured")
-// })
+// // const getData=()=>{
+// //     return new Promise((resolve,reject)=>{
+// //         console.log("I am a promise")
+// //         // resolve("Success message");
+// //         reject("Some error occured")
+// // })
+// // }
+
+// // let returnPromise=getData()
+// // returnPromise.then((res)=>{
+// //     console.log("Printing success")
+// // }).catch((err)=>{
+// //     console.log("some error occutred somcatch blocj print hrerte")
+// // })
+
+// //----------------------
+
+
+// //Promise channing
+
+// // function asyncFun1(){
+// //     return new Promise((resolve,reject)=>{
+// //         setTimeout(()=>{
+// //             console.log("Interval complete 1")
+// //             resolve("success1")
+// //         },4000)
+// //     })
+// // }
+
+// // function asyncFun2(){
+// //     return new Promise((resolve,reject)=>{
+// //         setTimeout(()=>{
+// //             console.log("Interval complete2")
+// //             resolve("success2")
+// //         },4000)
+// //     })
+// // }
+
+// // function asyncFun3(){
+// //     return new Promise((resolve,reject)=>{
+// //         setTimeout(()=>{
+// //             console.log("Interval complet3")
+// //             resolve("success3")
+// //         },4000)
+// //     })
+// // }
+// // console.log("Fetching Data 1")
+// // let result1=asyncFun1();
+// // result1.then((res)=>{
+// //     console.log(res);
+// //     console.log("Fetching data2")
+// //     let p2=asyncFun2();
+// //     p2.then((res)=>{
+// //         console.log(res);
+// //         console.log("Fetching data 3")
+// //         let p3=asyncFun3();
+// //         p3.then((res)=>{
+// //             console.log(res)
+// //         })
+// //     }).catch((err)=>{
+// //         console.log(err)
+// //     })
+// // }).catch((err)=>{
+// //     console.log(err)
+// // })
+
+// // --------------------------------------------------------------
+
+
+// //async-await
+
+// // async function hello (){
+// //     console.log("Hello world")
+// // }
+// // Function to convert currency
+// async function convertCurrency(amount, fromCurrency, toCurrency) {
+//     const apiKey = 'YOUR_API_KEY';  // Replace with your API key
+//     const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency}`;
+
+//     try {
+//         // Fetch exchange rates from the API
+//         const response = await fetch(apiUrl);
+//         const data = await response.json();
+
+//         if (data.result !== 'success') {
+//             throw new Error('Failed to fetch exchange rates');
+//         }
+
+//         // Get the conversion rate
+//         const rate = data.conversion_rates[toCurrency];
+//         if (!rate) {
+//             throw new Error(`Conversion rate for ${toCurrency} not available`);
+//         }
+
+//         // Convert the amount
+//         const convertedAmount = amount * rate;
+//         console.log(`${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`);
+//         return convertedAmount.toFixed(2);
+
+//     } catch (error) {
+//         console.error('Error:', error.message);
+//     }
 // }
 
-// let returnPromise=getData()
-// returnPromise.then((res)=>{
-//     console.log("Printing success")
-// }).catch((err)=>{
-//     console.log("some error occutred somcatch blocj print hrerte")
-// })
-
-//----------------------
-
-
-//Promise channing
-
-// function asyncFun1(){
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//             console.log("Interval complete 1")
-//             resolve("success1")
-//         },4000)
-//     })
-// }
-
-// function asyncFun2(){
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//             console.log("Interval complete2")
-//             resolve("success2")
-//         },4000)
-//     })
-// }
-
-// function asyncFun3(){
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//             console.log("Interval complet3")
-//             resolve("success3")
-//         },4000)
-//     })
-// }
-// console.log("Fetching Data 1")
-// let result1=asyncFun1();
-// result1.then((res)=>{
-//     console.log(res);
-//     console.log("Fetching data2")
-//     let p2=asyncFun2();
-//     p2.then((res)=>{
-//         console.log(res);
-//         console.log("Fetching data 3")
-//         let p3=asyncFun3();
-//         p3.then((res)=>{
-//             console.log(res)
-//         })
-//     }).catch((err)=>{
-//         console.log(err)
-//     })
-// }).catch((err)=>{
-//     console.log(err)
-// })
-
-// --------------------------------------------------------------
-
-
-//async-await
-
-// async function hello (){
-//     console.log("Hello world")
-// }
+// // Example usage
+// convertCurrency(100, 'USD', 'EUR')
+//     .then(convertedAmount => console.log(`Converted Amount: ${convertedAmount} EUR`))
+//     .catch(error => console.error('Conversion error:', error));
 
